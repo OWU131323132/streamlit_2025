15b56eb3942c92efc5efa2699c945324755925a8
OWU131323132 <s1323132@cst.otsuma.ac.jp>
2025-06-11 12:06:36 +0900

    Update test_0528.py

--- test_0528.py
+++ test_0528.py
@@ -1,6 +1,7 @@
-import streamlit as st
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+import streamlit as st
+
 
 st.set_page_config(page_title="食材からレシピを探すアプリ", layout="centered")
 
